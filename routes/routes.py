f150f196d8b1a01c7f7b378411a963e6b8a5f393
ANKIT1149 <140632363+ANKIT1149@users.noreply.github.com>
2025-02-28 09:02:56 +0530

    Setup Partially completed

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,6 @@
-from controller.AccountController import router
+from controller.AccountController import AccountRouter;
+from controller.UploadController import UploadRouter;
 
 def routes(app):
-    app.include_router(router, prefix="/account")
+    app.include_router(AccountRouter, prefix="/account")
+    app.include_router(UploadRouter)
